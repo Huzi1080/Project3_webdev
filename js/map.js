290617f6eb05bd400bbca48486e33c50b28f3d26
Huzi1080 <hanis1@hawk.iit.edu>
2025-04-16 21:08:32 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,15 @@
+function initMap() {
+  const location = { lat: 40.7128, lng: -74.0060 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: location,
+  });
+
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "We are here!",
+  });
+}
 
