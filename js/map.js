f90016615f44d4f966729de564141bbb0308e19a
Huzi1080 <hanis1@hawk.iit.edu>
2025-04-17 02:58:27 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,15 +1,35 @@
-function initMap() {
-  const location = { lat: 40.7128, lng: -74.0060 };
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Google Map</title>
+    <style>
+      #map {
+        height: 100vh;
+        width: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Our Location</h1>
+    <div id="map"></div>
 
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: location,
-  });
+    <script>
+      function initMap() {
+        const location = { lat: 40.7128, lng: -74.0060 }; 
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 12,
+          center: location,
+        });
 
-  const marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    title: "We are here!",
-  });
-}
+        new google.maps.Marker({
+          position: location,
+          map: map,
+        });
+      }
+    </script>
 
+    <script async
+      src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+    </script>
+  </body>
+</html>
